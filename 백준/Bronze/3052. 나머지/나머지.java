1b6a9d8eafe38de80384c17f8c7b8e5014db5081
B <143949216+urcloud@users.noreply.github.com>
2024-04-22 09:58:02 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<Integer> remainders = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            int num = scanner.nextInt();
+            remainders.add(num % 42);
+        }
+
+        System.out.println(remainders.size());
+    }
+}
