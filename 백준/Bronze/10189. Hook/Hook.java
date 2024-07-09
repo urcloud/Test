171967d15e3c466c4552eeb6fb0f42a5137aafb0
B <143949216+urcloud@users.noreply.github.com>
2024-07-09 17:05:36 +0900

    [Bronze V] Title: Hook, Time: 92 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] pattern = {
+            "#  # #### #### #  #",
+            "#### #  # #  # # #",
+            "#### #  # #  # # #",
+            "#  # #### #### #  #"
+        };
+        
+        for (String row : pattern) {
+            System.out.println(row);
+        }
+    }
+}
