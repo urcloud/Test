324ad57c407ea9b0a36018370b18d8f8c28cedd9
B <143949216+urcloud@users.noreply.github.com>
2024-12-18 07:58:31 +0900

    [Bronze V] Title: 구구단, Time: 200 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
 
-        int N = scanner.nextInt(); // N을 입력받음
+    Scanner scanner = new Scanner(System.in);
 
-        // 구구단 출력
-        for (int i = 1; i <= 9; i++) {
-            System.out.println(N + " * " + i + " = " + (N * i));
-        }
+    int N = scanner.nextInt();
+
+    for (int i = 0; i < 9; i++) {
+      System.out.println(N + " * " + (i+1) + " = " + (N * (i+1)));
     }
+
+    scanner.close();
+
+  }
 }
