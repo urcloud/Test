ac7acf713af6cc39ceaa154d5fb95c0957f1c7b2
B <143949216+urcloud@users.noreply.github.com>
2024-12-19 01:28:47 +0900

    [Bronze V] Title: 윤년, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // 연도 입력
-        int year = scanner.nextInt();
-
-        // 윤년 여부 확인
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println(1); // 윤년일 경우 1 출력
-        } else {
-            System.out.println(0); // 윤년이 아닐 경우 0 출력
-        }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    int year = scanner.nextInt();
+    
+    if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+      System.out.println(1);
+    } else {
+      System.out.println(0);
     }
+    
+    scanner.close();
+  }
 }
