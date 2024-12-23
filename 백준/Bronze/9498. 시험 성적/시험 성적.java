49deedd95884abc4b27a9fbf51b3c99880c9cdbb
B <143949216+urcloud@users.noreply.github.com>
2024-12-23 16:33:39 +0900

    [Bronze V] Title: 시험 성적, Time: 176 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int score = scanner.nextInt();
-        
-        char grade;
-        if (score >= 90 && score <= 100) {
-            grade = 'A';
-        } else if (score >= 80 && score <= 89) {
-            grade = 'B';
-        } else if (score >= 70 && score <= 79) {
-            grade = 'C';
-        } else if (score >= 60 && score <= 69) {
-            grade = 'D';
-        } else {
-            grade = 'F';
-        }
-        
-        System.out.println(grade);
-        scanner.close();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int N = scanner.nextInt();
+
+    if (90 <= N && N <= 100) {
+      System.out.println("A");
+    } else if (80 <= N && N <= 89) {
+      System.out.println("B");
+    } else if (70 <= N && N <= 79) {
+      System.out.println("C");
+    } else if (60 <= N && N <= 69) {
+      System.out.println("D");
+    } else {
+      System.out.println("F");
     }
+
+    scanner.close();
+  }
 }
