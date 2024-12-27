649d9d29156f5183b22804d026b234aff76bc26e
B <143949216+urcloud@users.noreply.github.com>
2024-12-27 21:48:08 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 344 ms, Memory: 20804 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    
+    BigInteger A = scanner.nextBigInteger();
+    BigInteger B = scanner.nextBigInteger();
+    BigInteger C = A.add(B);
+
+    System.out.println(C);
+  }
+}
