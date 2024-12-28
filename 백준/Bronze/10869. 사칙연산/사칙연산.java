7411f76d6612995d86d2ede27a0aed77f6403003
B <143949216+urcloud@users.noreply.github.com>
2024-12-28 19:13:00 +0900

    [Bronze V] Title: 사칙연산, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,24 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // 두 자연수 A와 B를 입력받음
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        
-        // A+B 출력
-        System.out.println(A + B);
-        // A-B 출력
-        System.out.println(A - B);
-        // A*B 출력
-        System.out.println(A * B);
-        // A/B 출력 (몫)
-        System.out.println(A / B);
-        // A%B 출력 (나머지)
-        System.out.println(A % B);
-        
-        scanner.close();
-    }
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int A = scanner.nextInt();
+    int B = scanner.nextInt();
+    
+    System.out.println(A+B);
+    System.out.println(A-B);
+    System.out.println(A*B);
+    System.out.println(A/B);
+    System.out.println(A%B);
+    scanner.close();
+  }
 }
