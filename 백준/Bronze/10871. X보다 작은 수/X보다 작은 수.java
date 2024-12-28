72f5c409c5828bbd21dd4af5f4c4d895201526fa
B <143949216+urcloud@users.noreply.github.com>
2024-12-28 19:16:57 +0900

    [Bronze V] Title: X보다 작은 수, Time: 444 ms, Memory: 30124 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int N = scanner.nextInt(); // N 입력
-        int X = scanner.nextInt(); // X 입력
-        
-        ArrayList<Integer> A = new ArrayList<>(); // 수열 A를 저장할 ArrayList 생성
-        
-        // 수열 A를 입력받아 ArrayList에 저장
-        for (int i = 0; i < N; i++) {
-            int num = scanner.nextInt();
-            A.add(num);
-        }
-        
-        // X보다 작은 수를 출력
-        for (int num : A) {
-            if (num < X) {
-                System.out.print(num + " ");
-            }
-        }
-        
-        scanner.close();
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int N = scanner.nextInt();
+    int X = scanner.nextInt();
+      
+    int[] A = new int[N];
+      
+    for (int i=0; i<N; i++) {
+        A[i] = scanner.nextInt();
     }
+      
+    for (int j=0; j<N; j++) {
+        if(A[j] < X) {
+            System.out.println(A[j]);
+        }
+    }
+      
+    scanner.close();
+  }
 }
