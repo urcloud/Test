c1a9701ab2850ac03246e417019f4d2d57421776
B <143949216+urcloud@users.noreply.github.com>
2024-12-30 08:52:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 116 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
-    int A = scanner.next().charAt(0);
-
-    System.out.println(A);
-  }
+	public static void main(String[] args) throws Exception {
+ 
+		int a = System.in.read();
+		System.out.print(a);
+ 
+	}
 }
