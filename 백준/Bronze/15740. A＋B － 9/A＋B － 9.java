51d126c96448c348f50c2b24febd1c97d0155fa5
B <143949216+urcloud@users.noreply.github.com>
2025-01-10 15:33:55 +0900

    [Bronze V] Title: A+B - 9, Time: 216 ms, Memory: 17392 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Long A = scanner.nextLong();
-        Long B = scanner.nextLong();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
         
-        System.out.println(A+B);
-        
-        scanner.close();
+        BigInteger a = new BigInteger(split[0]);
+        BigInteger b = new BigInteger(split[1]);
+
+        System.out.println(a.add(b));
     }
 }
