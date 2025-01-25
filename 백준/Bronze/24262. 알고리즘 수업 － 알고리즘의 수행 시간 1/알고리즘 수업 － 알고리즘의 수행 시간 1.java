ee31c4266121b89733f9d84317c9a85031507349
B <143949216+urcloud@users.noreply.github.com>
2025-01-25 10:55:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //입력값 받아서 출력하는 함수이므로 수행횟수는 무조건 1
+        System.out.println(1);
+        
+        //입력과 무관한 시간복잡도 함수는 O(1), 상수항이므로 차수가 0
+        System.out.println(0);
+    }
+}
