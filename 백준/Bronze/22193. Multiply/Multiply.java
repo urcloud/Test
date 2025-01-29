c1d50bb4f2ece401055177480b0e0e0812e48fdf
B <143949216+urcloud@users.noreply.github.com>
2025-01-29 19:10:33 +0900

    [Bronze V] Title: Multiply, Time: 780 ms, Memory: 60328 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int M = scanner.nextInt();
-        long A = scanner.nextLong();
-        long B = scanner.nextLong();
+        BigInteger A = scanner.nextBigInteger();
+        BigInteger B = scanner.nextBigInteger();
         
-        long result = A*B;
+        BigInteger result = A.multiply(B);
         
         System.out.println(result);
         
