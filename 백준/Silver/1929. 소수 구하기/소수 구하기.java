5e34879b6f5b50998a3100576d456e803d4175e2
B <143949216+urcloud@users.noreply.github.com>
2025-02-03 15:31:23 +0900

    [Silver III] Title: 소수 구하기, Time: 716 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// 에라토스테네스의 체
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int M = scanner.nextInt();
+        int N = scanner.nextInt();
+        scanner.close();
+
+        boolean[] isPrime = new boolean[N + 1];
+
+        for (int i = 2; i <= N; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
