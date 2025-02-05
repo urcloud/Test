b935c57b680ac4252715f2a787e1048a1a11f13e
B <143949216+urcloud@users.noreply.github.com>
2025-02-05 15:31:50 +0900

    [Silver V] Title: 창문 닫기, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.close();
+
+        System.out.println((int) Math.sqrt(N));
+    }
+}
