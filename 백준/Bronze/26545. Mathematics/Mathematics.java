eddd4c31c067257deb66211519f28025c2b0cb76
B <143949216+urcloud@users.noreply.github.com>
2025-02-07 15:55:00 +0900

    [Bronze V] Title: Mathematics, Time: 160 ms, Memory: 17320 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        int result = 0;
+        
+        for(int i=0; i<n; i++) {
+            a[i] = scanner.nextInt();
+            result += a[i];
+        }
+        
+        System.out.println(result);
+        scanner.close();
+    }
+}
