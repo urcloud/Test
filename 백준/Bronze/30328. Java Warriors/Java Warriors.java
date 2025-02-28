5a245a9b3d4f506dce1628cbceb3028aa595a9a8
B <143949216+urcloud@users.noreply.github.com>
2025-02-28 23:40:38 +0900

    [Bronze V] Title: Java Warriors, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        System.out.println(N*4000);
+        scanner.close();
+    }
+}
