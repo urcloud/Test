f10335fdd5f4c6f5cd471a4c315e05b03fa6d91d
B <143949216+urcloud@users.noreply.github.com>
2025-03-05 14:16:02 +0900

    [Bronze V] Title: Adding Trouble, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        
+        if(A+B==C) {
+            System.out.println("correct!");
+        } else {
+            System.out.println("wrong!");
+        }
+        
+        scanner.close();
+    }
+}
