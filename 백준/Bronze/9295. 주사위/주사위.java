19d457fae3bc96da42332b3289b4fbe7435dd5ac
B <143949216+urcloud@users.noreply.github.com>
2025-03-21 21:31:06 +0900

    [Bronze IV] Title: 주사위, Time: 320 ms, Memory: 22728 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[][] num = new int[n][2];
+
+		for(int i=0; i<n; i++) {
+			num[i][0] = scanner.nextInt();
+			num[i][1] = scanner.nextInt();
+
+			int result = num[i][0] + num[i][1];
+			System.out.println("Case " + (i+1) +": " + result);
+		}
+
+		scanner.close();
+	}
+}
