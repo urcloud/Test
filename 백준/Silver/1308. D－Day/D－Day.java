7851d686c4f82bb12dd50991e0e736d75708bbd5
B <143949216+urcloud@users.noreply.github.com>
2025-03-25 10:25:00 +0900

    [Silver V] Title: D-Day, Time: 184 ms, Memory: 18408 KB -BaekjoonHub

---
+++ 백준/Silver/1308. D－Day/D－Day.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int y1 = scanner.nextInt();
+        int m1 = scanner.nextInt();
+        int d1 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        int m2 = scanner.nextInt();
+        int d2 = scanner.nextInt();
+
+        int total1 = calcDay(y1, m1, d1);
+        int total2 = calcDay(y2, m2, d2);
+        
+        if(y2 - y1 > 1000 || y2 - y1 == 1000 && (m1 < m2 || m1 == m2 && d1 <= d2)) System.out.println("gg");
+        else System.out.println("D-" + (total2 - total1));
+    }
+
+    static int calcDay(int y, int m, int d){
+        int[] day = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int days = 0;
+
+        for(int i = 1; i < y; i++){
+            days += 365 + checkYear(i);
+        }
+
+        for(int i = 1; i < m; i++){
+            if(i == 2) days += checkYear(y);
+            days += day[i];
+        }
+
+        days += d;
+
+        return days;
+    }
+
+    static int checkYear(int y){
+        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) return 1;
+        else return 0;
+    }
+}
