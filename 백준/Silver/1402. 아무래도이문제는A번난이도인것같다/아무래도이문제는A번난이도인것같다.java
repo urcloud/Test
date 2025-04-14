2afd265e20bb9066378f3f09ddf4df881bbd2dc4
B <143949216+urcloud@users.noreply.github.com>
2025-04-14 10:05:27 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int cnt = Integer.parseInt(br.readLine());
+	    for(int i = 0; i < cnt; i++){
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.println("yes");
+	    }
+	    	    
+	}
+}
